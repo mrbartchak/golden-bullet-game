d2ddbb6704155fed83e05644dc77cff36f600acf
mrbartchak <matthewbartchak@gmail.com>
2026-01-06 12:29:46 -0800

    base stuff like player, bullet firing, etc, init commit ya momma

---
+++ bullets/bullet.gd
@@ -0,0 +1,33 @@
+class_name Bullet
+extends Area2D
+
+@export var speed: float = 900.0
+@export var damage: int = 1
+@export var lifetime: float = 3.0
+
+var velocity: Vector2
+var alive: bool = true
+
+func _ready() -> void:
+	monitoring = true
+	
+	if lifetime > 0:
+		await get_tree().create_timer(lifetime).timeout
+		queue_free()
+
+func _physics_process(delta: float) -> void:
+	position += velocity * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	if not alive:
+		return
+	if not body.is_in_group("enemies"):
+		return
+	
+	alive = false
+	set_deferred("monitoring", false)
+	
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	
+	queue_free()
---
+++ player/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@export var movement_speed: float = 70.0
+@export var bullet_scene: PackedScene
+var muzzle_offset: int = 4
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("fire"):
+		fire()
+
+
+func _physics_process(_delta: float) -> void:
+	_handle_movement()
+
+
+func fire() -> void:
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	var direction: Vector2 = (mouse_pos - global_position).normalized()
+	
+	var bullet: Bullet = bullet_scene.instantiate()
+	bullet.global_position = global_position + (direction * muzzle_offset)
+	bullet.velocity = direction * bullet.speed
+	
+	get_tree().current_scene.add_child(bullet)
+
+
+func _handle_movement() -> void:
+	var input_dir: Vector2 = Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	)
+	
+	velocity = input_dir * movement_speed
+	move_and_slide()
